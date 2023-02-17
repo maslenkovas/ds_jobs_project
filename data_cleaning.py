a94810c874eb8fa235d5a3245ac80b28b69a8feb
Светлана Масленкова <svetlanamaslenkova@Svetlanas-MacBook-Air.local>
2023-02-17 16:13:25 +0400

    data_ccleaning file edited, df saved as csv

--- data_cleaning.py
+++ data_cleaning.py
@@ -91,48 +91,25 @@ words = re.findall(r'\w+', result_text)
 two_words = [' '.join(ws) for ws in zip(words, words[1:])]
 wordscount = {w:f for w, f in Counter(two_words).most_common() if f > 1}
 
-# # add experience level column
+# add experience level column where 0 : no info, 1 : junior, 2 : senior
 # jobs_with_salary_df['junior_lvl'] = jobs_with_salary_df['job_title'].apply(lambda x: \
 #             1 if 'junior' in x.lower() else 0)
 # jobs_with_salary_df['senior_lvl'] = jobs_with_salary_df['job_title'].apply(lambda x: \
 #             1 if 'senior' in x.lower() or 'expert' in x.lower()  else 0)
 jobs_with_salary_df['experience_level'] = jobs_with_salary_df['job_title'].apply(lambda x: \
-            1 if 'junior' in x.lower() else 2 if 'senior' in x.lower() or 'expert' in x.lower()\
-                or 'lead' in x.lower()  else 0)
+            'junior' if 'junior' in x.lower() or 'jr.' in x.lower() else 'senior' if 'senior' in x.lower() or 'expert' in x.lower()\
+                or 'lead' in x.lower() or 'principal' in x.lower() or 'sr.' in x.lower()  else 'na')
 
-# # rearrange the columns
-# jobs_with_salary_df = jobs_with_salary_df[['job_title',
-#  'salary_estimate',
-#  'job_description',
-#  'rating',
-#  'company_name',
-#  'location',
-#  'job_age',
-#  'size',
-#  'founded',
-#  'type_of_ownership',
-#  'industry',
-#  'sector',
-#  'revenue',
-#  'competitors',
-#  'date_scrapped',
-#  'job_fam',
-#  'salary_description',
-#  'hourly',
-#  'annually',
-#  'salary',
-#  'currency',
-#  'min_salary',
-#  'max_salary',
-#  'avg_salary',
-#  'company_age',
-#  'python_yn',
-#  'r_yn',
-#  'spark_yn',
-#  'aws_yn',
-#  'pytorch_yn',
-#  'tf_yn',
-#  'ml_yn',
-#  'experience_level'
-# ]]
+# add description length column
+jobs_with_salary_df['desc_len'] = jobs_with_salary_df.job_description.apply(lambda x: len(x))
+
+# get the min and max salary unified as monthly salaries
+jobs_with_salary_df['min_salary'] = jobs_with_salary_df.apply(lambda x: x.min_salary*160 if x.hourly==1 else x.min_salary/12 if x.annually==1 else x.min_salary, axis=1)
+jobs_with_salary_df['max_salary'] = jobs_with_salary_df.apply(lambda x: x.max_salary*160 if x.hourly==1 else x.max_salary/12 if x.annually==1 else x.max_salary, axis=1)
+jobs_with_salary_df['avg_salary'] = jobs_with_salary_df.apply(lambda x: x.avg_salary*160 if x.hourly==1 else x.avg_salary/12 if x.annually==1 else x.avg_salary, axis=1)
+
+# remove rating and new line character from company_name column
+jobs_with_salary_df['company_name'] = jobs_with_salary_df['company_name'].apply(lambda x: x.split('\n')[0] if len(x.split('\n'))>1 else x)
+
+    
 jobs_with_salary_df.to_csv('glassdoor_jobs.csv', index=False)
